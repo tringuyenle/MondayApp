592ddd58ce4c667f285b14ed7c70da0fb82bf0e6
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-06-16 16:30:39 +0700

    update group component

--- ClientApp/src/app/group/expandgroup/expandgroup.component.ts
+++ ClientApp/src/app/group/expandgroup/expandgroup.component.ts
@@ -20,20 +20,8 @@ export class ExpandgroupComponent implements OnInit {
     this.collapseeChange.emit(true);
   }
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX - The Rise of Skywalker',
-  ];
-
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.task_list, event.previousIndex, event.currentIndex);
   }
 
   ngOnInit(): void {
