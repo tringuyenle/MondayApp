09cf5670a4531d072bb4c3e0581517cf7e715944
Thi Khắc Quân <107615902+BlueSky-17@users.noreply.github.com>
2023-06-19 08:32:51 +0700

    add and update task

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -11,18 +11,15 @@ import { HeaderComponent } from './header/header.component';
 import { BoardcontentComponent } from './boardcontent/boardcontent.component';
 import { StatuslabelComponent } from './statuslabel/statuslabel.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-<<<<<<< Updated upstream
 import { BoardinfoComponent } from './boardinfo/boardinfo.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { CollapsegroupComponent } from './group/collapsegroup/collapsegroup.component';
 import { ExpandgroupComponent } from './group/expandgroup/expandgroup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-=======
-import { TaskListComponent } from './boardcontent/task-list/task-list.component';
 import { AddTaskComponent } from './boardcontent/add-task/add-task.component';
 import { TaskRoutingModule } from './task-routing.module';
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
@@ -34,27 +31,20 @@ import { TaskRoutingModule } from './task-routing.module';
     StatuslabelComponent,
     SidebarComponent,
     TaskListComponent,
-<<<<<<< Updated upstream
     CollapsegroupComponent,
-    
-=======
     AddTaskComponent,
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-<<<<<<< Updated upstream
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-    ]),
+    // RouterModule.forRoot([
+    //   { path: '', component: HomeComponent, pathMatch: 'full' },
+    // ]),
     BrowserAnimationsModule,
-    ExpandgroupComponent
-=======
+    ExpandgroupComponent,
     TaskRoutingModule,
     ReactiveFormsModule
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/task-routing.module.ts
+++ ClientApp/src/app/task-routing.module.ts
@@ -2,7 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './boardcontent/task-list/task-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
 import { AddTaskComponent } from './boardcontent/add-task/add-task.component';
 import { BoardcontentComponent } from './boardcontent/boardcontent.component';
 
