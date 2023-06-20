ce8ec5c4cb5bd2b8137b148078b2c8f149e3b345
Thi Khắc Quân <107615902+BlueSky-17@users.noreply.github.com>
2023-06-20 09:52:50 +0700

    edit task

--- ClientApp/src/app/group/expandgroup/expandgroup.component.ts
+++ ClientApp/src/app/group/expandgroup/expandgroup.component.ts
@@ -2,27 +2,29 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
 import {CdkListbox, CdkOption} from '@angular/cdk/listbox';
 import {NgFor, JsonPipe} from '@angular/common';
-import { Task } from 'src/app/task';
-import { TaskService } from 'src/services/task-service/task.service';
 import { FormBuilder, FormGroup, FormsModule, FormControlName, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TaskListService } from 'src/services/task-list-service/task-list.service';
 import { AddTaskService } from 'src/services/add-task-service/add-task.service';
+import { NgIf } from '@angular/common';
+import { EditTaskService } from 'src/services/edit-task-service/edit-task.service';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-expandgroup',
   templateUrl: './expandgroup.component.html',
   styleUrls: ['./expandgroup.component.css'],
   standalone: true,
-  imports: [CdkDropList, NgFor, CdkDrag, FormsModule, ReactiveFormsModule, CdkListbox, CdkOption, JsonPipe],
-  providers: [TaskListService, AddTaskService],
+  imports: [CdkDropList, NgFor, CdkDrag, FormsModule, ReactiveFormsModule, CdkListbox, CdkOption, JsonPipe, NgIf, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  // providers: [TaskListService, AddTaskService,],
 })
 export class ExpandgroupComponent implements OnInit {
   @Input() collapsee: boolean = false;
   @Output() collapseeChange = new EventEmitter<boolean>();
-  task_list: Task[] = [];
-  task_form!: FormGroup;
-  task!: Task;
   languages = ['C++', 'Java', 'JavaScript', 'Python', 'TypeScript'];
   languageCtrl = new FormControl(['TypeScript']);
 
@@ -39,7 +41,7 @@ export class ExpandgroupComponent implements OnInit {
     this.add_task_service.buildForm('');
   }
 
-  constructor(public task_list_service: TaskListService, public add_task_service: AddTaskService) {}
+  constructor(public task_list_service: TaskListService, public add_task_service: AddTaskService, public edit_task_service: EditTaskService) {}
 
 }
 function output() {
---
+++ ClientApp/src/services/edit-task-service/edit-task.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { TaskService } from '../task-service/task.service';
+import { Task } from 'src/app/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditTaskService {
+
+  edit_task_id: string = '';
+
+  constructor(private task_service: TaskService) { }
+
+  editCell(task: Task): void {
+    this.edit_task_id = task.id;
+   }
+ 
+   saveCell(task: Task): void {
+     this.task_service.updateTask(task).subscribe({
+       error: () => {
+         this.edit_task_id = '';
+         console.log("Chỉnh sửa thành công");
+       }
+     })
+   }
+}
