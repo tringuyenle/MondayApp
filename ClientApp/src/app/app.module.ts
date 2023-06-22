f286aa7539894aeee61399afef064c6200418678
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-06-22 14:59:56 +0700

    update sidebar

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -27,6 +27,9 @@ import en from '@angular/common/locales/en';
 import {MatTreeModule} from '@angular/material/tree';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import {CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
 import {CdkListbox, CdkOption} from '@angular/cdk/listbox';
@@ -62,7 +65,8 @@ registerLocaleData(en);
     MatTreeModule,
     MatIconModule,
     MatTableModule,
-    CdkDropList, NgFor, CdkDrag, FormsModule, ReactiveFormsModule, CdkListbox, CdkOption, JsonPipe, NgIf, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule
+    CdkDropList, NgFor, CdkDrag, FormsModule, ReactiveFormsModule, CdkListbox, CdkOption, JsonPipe, NgIf, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
+    MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule,
   ],
   bootstrap: [AppComponent]
 })
