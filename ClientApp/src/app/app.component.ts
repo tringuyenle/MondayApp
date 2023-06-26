be2d37907ce5d79216ce75a78e8fa52e3b65ab0b
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-06-26 16:51:22 +0700

    fix chut xiu

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,42 +1,10 @@
 import { Component } from '@angular/core';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule } from '@angular/material/tree';
-import { MatTableModule } from '@angular/material/table';
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-interface ExampleFlatNode{
-  expandable: boolean;
-  name: string;
-  level: number;
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}]
-  },
-  {
-    name: 'Vegetable',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
+import { Grouptask } from './grouptask';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  firstgroup!: Grouptask;
 }
