78441e5f9dce31f826473aea97135d104673fa78
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-06-27 10:05:21 +0700

    fix sidenav mode to open

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  @Input() opened: boolean = true;
 }
