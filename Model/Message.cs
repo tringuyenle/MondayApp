104d96431fe9356a67d783a5a02be1e89c4c2d54
Thi Khắc Quân <107615902+BlueSky-17@users.noreply.github.com>
2023-06-28 08:45:22 +0700

    add message to backend

---
+++ Model/Message.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MondayApp.Model
+{
+    public class Message
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? id { get; set; }
+        public string? parent_id { get; set; }
+        public string? reply_id { get; set; }
+        public string content { get; set; }
+        public string time { get; set; }
+    }
+}
